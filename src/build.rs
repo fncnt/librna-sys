34bb1bc71b7fac7a5640fcfa6354e5031ff3ab44
Vincent Messow <github@vncnt.eu>
2020-08-21 12:02:17 +0200

    Move build.rs and wrapper.h into src/.

--- src/build.rs
+++ src/build.rs
@@ -56,7 +56,7 @@ fn main() {
     println!("cargo:rustc-link-lib=RNA");
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
-    println!("cargo:rerun-if-changed=wrapper.h");
+    println!("cargo:rerun-if-changed=src/wrapper.h");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -64,7 +64,7 @@ fn main() {
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
-        .header("wrapper.h")
+        .header("src/wrapper.h")
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         //.parse_callbacks(Box::new(bindgen::CargoCallbacks))
