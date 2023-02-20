a070ff1d54b6b3ab76ccf5d9d8e2400cb9e614e4
Vincent Messow <github@vncnt.eu>
2023-02-20 15:10:03 +0100

    Minor annotations of wrapped headers

--- src/wrapper.h
+++ src/wrapper.h
@@ -1,14 +1,7 @@
 #include <ViennaRNA/2Dfold.h>
 #include <ViennaRNA/2Dpfold.h>
-#include <ViennaRNA/LPfold.h>
-#include <ViennaRNA/Lfold.h>
-#include <ViennaRNA/MEA.h>
-#include <ViennaRNA/PKplex.h>
-#include <ViennaRNA/PS_dot.h>
-#include <ViennaRNA/ProfileAln.h>
-#include <ViennaRNA/RNAstruct.h>
-#include <ViennaRNA/ali_plex.h>
 #include <ViennaRNA/alifold.h>
+#include <ViennaRNA/ali_plex.h>
 #include <ViennaRNA/aln_util.h>
 #include <ViennaRNA/alphabet.h>
 #include <ViennaRNA/boltzmann_sampling.h>
@@ -18,20 +11,20 @@
 #include <ViennaRNA/combinatorics.h>
 #include <ViennaRNA/commands.h>
 #include <ViennaRNA/concentrations.h>
-#include <ViennaRNA/constraints.h>
-#include <ViennaRNA/constraints/SHAPE.h>
 #include <ViennaRNA/constraints/basic.h>
+#include <ViennaRNA/constraints.h>
 #include <ViennaRNA/constraints/hard.h>
-#include <ViennaRNA/constraints/ligand.h>
-#include <ViennaRNA/constraints/soft.h>
-#include <ViennaRNA/constraints_SHAPE.h>
 #include <ViennaRNA/constraints_hard.h>
+#include <ViennaRNA/constraints/ligand.h>
 #include <ViennaRNA/constraints_ligand.h>
+#include <ViennaRNA/constraints/SHAPE.h>
+#include <ViennaRNA/constraints_SHAPE.h>
+#include <ViennaRNA/constraints/soft.h>
 #include <ViennaRNA/constraints_soft.h>
 #include <ViennaRNA/convert_epars.h>
-#include <ViennaRNA/data_structures.h>
 #include <ViennaRNA/datastructures/basic.h>
 #include <ViennaRNA/datastructures/char_stream.h>
+#include <ViennaRNA/data_structures.h>
 #include <ViennaRNA/datastructures/hash_tables.h>
 #include <ViennaRNA/datastructures/heap.h>
 #include <ViennaRNA/datastructures/lists.h>
@@ -49,13 +42,14 @@
 #include <ViennaRNA/file_formats_msa.h>
 #include <ViennaRNA/file_utils.h>
 #include <ViennaRNA/findpath.h>
-#include <ViennaRNA/fold.h>
 #include <ViennaRNA/fold_compound.h>
+#include <ViennaRNA/fold.h>
 #include <ViennaRNA/fold_vars.h>
 #include <ViennaRNA/utils/basic.h>
 #include <ViennaRNA/gquad.h>
 #include <ViennaRNA/grammar.h>
 #include <ViennaRNA/hairpin_loops.h>
+#include <ViennaRNA/heat_capacity.h>
 #include <ViennaRNA/interior_loops.h>
 #include <ViennaRNA/inverse.h>
 #include <ViennaRNA/io/file_formats.h>
@@ -67,12 +61,15 @@
 #include <ViennaRNA/landscape/neighbor.h>
 #include <ViennaRNA/landscape/paths.h>
 #include <ViennaRNA/landscape/walk.h>
+#include <ViennaRNA/Lfold.h>
 #include <ViennaRNA/loop_energies.h>
 #include <ViennaRNA/loops/all.h>
 #include <ViennaRNA/loops/external.h>
 #include <ViennaRNA/loops/hairpin.h>
 #include <ViennaRNA/loops/internal.h>
 #include <ViennaRNA/loops/multibranch.h>
+#include <ViennaRNA/LPfold.h>
+#include <ViennaRNA/MEA.h>
 #include <ViennaRNA/mfe.h>
 #include <ViennaRNA/mfe_window.h>
 #include <ViennaRNA/mm.h>
@@ -82,55 +79,70 @@
 #include <ViennaRNA/naview.h>
 #include <ViennaRNA/neighbor.h>
 #include <ViennaRNA/pair_mat.h>
-#include <ViennaRNA/params.h>
 #include <ViennaRNA/params/basic.h>
 #include <ViennaRNA/params/constants.h>
 #include <ViennaRNA/params/convert.h>
 #include <ViennaRNA/params/default.h>
+#include <ViennaRNA/params.h>
 #include <ViennaRNA/params/io.h>
-#include <ViennaRNA/part_func.h>
 #include <ViennaRNA/part_func_co.h>
+#include <ViennaRNA/part_func.h>
 #include <ViennaRNA/part_func_up.h>
 #include <ViennaRNA/part_func_window.h>
 #include <ViennaRNA/perturbation_fold.h>
+#include <ViennaRNA/PKplex.h>
 #include <ViennaRNA/plex.h>
 #include <ViennaRNA/plot_aln.h>
 #include <ViennaRNA/plot_layouts.h>
 #include <ViennaRNA/plot_structure.h>
-#include <ViennaRNA/plot_utils.h>
-#include <ViennaRNA/plotting/RNApuzzler/RNApuzzler.h>
-#include <ViennaRNA/plotting/RNApuzzler/RNAturtle.h>
 #include <ViennaRNA/plotting/alignments.h>
 #include <ViennaRNA/plotting/layouts.h>
-#include <ViennaRNA/plotting/naview.h>
+// This was changed to #include <ViennaRNA/plotting/naview/naview.h> in ViennaRNA 2.5.1
+// We'll remove this include for now since #include <ViennaRNA/naview.h> is still available (although deprecated)
+// #include <ViennaRNA/plotting/naview.h>
 #include <ViennaRNA/plotting/probabilities.h>
+#include <ViennaRNA/plotting/RNApuzzler/RNApuzzler.h>
+#include <ViennaRNA/plotting/RNApuzzler/RNAturtle.h>
 #include <ViennaRNA/plotting/structures.h>
 #include <ViennaRNA/plotting/utils.h>
+#include <ViennaRNA/plot_utils.h>
+#include <ViennaRNA/ProfileAln.h>
 #include <ViennaRNA/profiledist.h>
+#include <ViennaRNA/PS_dot.h>
 #include <ViennaRNA/read_epars.h>
 #include <ViennaRNA/ribo.h>
+#include <ViennaRNA/RNAstruct.h>
 #include <ViennaRNA/search/BoyerMoore.h>
 #include <ViennaRNA/sequence.h>
 #include <ViennaRNA/snofold.h>
 #include <ViennaRNA/snoop.h>
 #include <ViennaRNA/stream_output.h>
-#include <ViennaRNA/string_utils.h>
 #include <ViennaRNA/stringdist.h>
-#include <ViennaRNA/structure_utils.h>
+#include <ViennaRNA/string_utils.h>
 #include <ViennaRNA/structured_domains.h>
+#include <ViennaRNA/structure_utils.h>
 #include <ViennaRNA/subopt.h>
 #include <ViennaRNA/svm.h>
 #include <ViennaRNA/svm_utils.h>
 #include <ViennaRNA/treedist.h>
 #include <ViennaRNA/units.h>
 #include <ViennaRNA/unstructured_domains.h>
-//#include <ViennaRNA/utils.h>
 #include <ViennaRNA/utils/alignments.h>
-//#include <ViennaRNA/utils/basic.h>
+// Moved this up in front of gquad.h to avoid implicit definition of vrna_alloc()
+// #include <ViennaRNA/utils/basic.h>
 #include <ViennaRNA/utils/cpu.h>
+#include <ViennaRNA/utils.h>
 #include <ViennaRNA/utils/higher_order_functions.h>
 #include <ViennaRNA/utils/strings.h>
 #include <ViennaRNA/utils/structures.h>
 #include <ViennaRNA/utils/svm.h>
+#include <ViennaRNA/utils/units.h>
 #include <ViennaRNA/vrna_config.h>
 #include <ViennaRNA/walk.h>
+#include <ViennaRNA/zscore.h>
+// new since 2.4.18:
+// #include <ViennaRNA/pk_plex.h>
+// new since 2.5.0
+// include <ViennaRNA/pf_multifold.h>
+// #include <ViennaRNA/subopt_zuker.h>
+// #include <ViennaRNA/wrap_dlib.h>
