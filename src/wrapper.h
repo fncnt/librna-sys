f6553b59886a6a465445da98818736c129128414
Vincent Messow <github@vncnt.eu>
2024-09-09 12:29:06 +0200

    Don't use (completely) deprecated header files with bindgen

--- src/wrapper.h
+++ src/wrapper.h
@@ -2,29 +2,29 @@
 #include <ViennaRNA/2Dpfold.h>
 #include <ViennaRNA/alifold.h>
 #include <ViennaRNA/ali_plex.h>
-#include <ViennaRNA/aln_util.h>
+// DEPRECATED: #include <ViennaRNA/aln_util.h>
 #include <ViennaRNA/alphabet.h>
 #include <ViennaRNA/boltzmann_sampling.h>
 #include <ViennaRNA/centroid.h>
-#include <ViennaRNA/char_stream.h>
+// DEPRECATED: #include <ViennaRNA/char_stream.h>
 #include <ViennaRNA/cofold.h>
 #include <ViennaRNA/combinatorics.h>
 #include <ViennaRNA/commands.h>
 #include <ViennaRNA/concentrations.h>
 #include <ViennaRNA/constraints/basic.h>
-#include <ViennaRNA/constraints.h>
+// DEPRECATED: #include <ViennaRNA/constraints.h>
 #include <ViennaRNA/constraints/hard.h>
-#include <ViennaRNA/constraints_hard.h>
+// DEPRECATED: #include <ViennaRNA/constraints_hard.h>
 #include <ViennaRNA/constraints/ligand.h>
-#include <ViennaRNA/constraints_ligand.h>
+// DEPRECATED: #include <ViennaRNA/constraints_ligand.h>
 #include <ViennaRNA/constraints/SHAPE.h>
-#include <ViennaRNA/constraints_SHAPE.h>
+// DEPRECATED: #include <ViennaRNA/constraints_SHAPE.h>
 #include <ViennaRNA/constraints/soft.h>
-#include <ViennaRNA/constraints_soft.h>
-#include <ViennaRNA/convert_epars.h>
+// DEPRECATED: #include <ViennaRNA/constraints_soft.h>
+// DEPRECATED: #include <ViennaRNA/convert_epars.h>
 #include <ViennaRNA/datastructures/basic.h>
 #include <ViennaRNA/datastructures/char_stream.h>
-#include <ViennaRNA/data_structures.h>
+// DEPRECATED: #include <ViennaRNA/data_structures.h>
 #include <ViennaRNA/datastructures/hash_tables.h>
 #include <ViennaRNA/datastructures/heap.h>
 #include <ViennaRNA/datastructures/lists.h>
@@ -33,24 +33,23 @@
 #include <ViennaRNA/dp_matrices.h>
 #include <ViennaRNA/duplex.h>
 #include <ViennaRNA/edit_cost.h>
-#include <ViennaRNA/energy_const.h>
-#include <ViennaRNA/energy_par.h>
+// DEPRECATED: #include <ViennaRNA/energy_const.h>
+// DEPRECATED: #include <ViennaRNA/energy_par.h>
 #include <ViennaRNA/equilibrium_probs.h>
 #include <ViennaRNA/eval.h>
-#include <ViennaRNA/exterior_loops.h>
-#include <ViennaRNA/file_formats.h>
-#include <ViennaRNA/file_formats_msa.h>
-#include <ViennaRNA/file_utils.h>
-#include <ViennaRNA/findpath.h>
+// DEPRECATED: #include <ViennaRNA/exterior_loops.h>
+// DEPRECATED: #include <ViennaRNA/file_formats.h>
+// DEPRECATED: #include <ViennaRNA/file_formats_msa.h>
+// DEPRECATED: #include <ViennaRNA/file_utils.h>
+// DEPRECATED: #include <ViennaRNA/findpath.h>
 #include <ViennaRNA/fold_compound.h>
 #include <ViennaRNA/fold.h>
 #include <ViennaRNA/fold_vars.h>
-#include <ViennaRNA/utils/basic.h>
-#include <ViennaRNA/gquad.h>
+// DEPRECATED: #include <ViennaRNA/gquad.h>
 #include <ViennaRNA/grammar.h>
-#include <ViennaRNA/hairpin_loops.h>
+// DEPRECATED: #include <ViennaRNA/hairpin_loops.h>
 #include <ViennaRNA/heat_capacity.h>
-#include <ViennaRNA/interior_loops.h>
+// DEPRECATED: #include <ViennaRNA/interior_loops.h>
 #include <ViennaRNA/inverse.h>
 #include <ViennaRNA/io/file_formats.h>
 #include <ViennaRNA/io/file_formats_msa.h>
@@ -62,7 +61,7 @@
 #include <ViennaRNA/landscape/paths.h>
 #include <ViennaRNA/landscape/walk.h>
 #include <ViennaRNA/Lfold.h>
-#include <ViennaRNA/loop_energies.h>
+// DEPRECATED: #include <ViennaRNA/loop_energies.h>
 #include <ViennaRNA/loops/all.h>
 #include <ViennaRNA/loops/external.h>
 #include <ViennaRNA/loops/hairpin.h>
@@ -75,70 +74,67 @@
 #include <ViennaRNA/mm.h>
 #include <ViennaRNA/model.h>
 #include <ViennaRNA/move_set.h>
-#include <ViennaRNA/multibranch_loops.h>
-#include <ViennaRNA/naview.h>
-#include <ViennaRNA/neighbor.h>
+// DEPRECATED: #include <ViennaRNA/multibranch_loops.h>
+// DEPRECATED: #include <ViennaRNA/naview.h>
+// DEPRECATED: #include <ViennaRNA/neighbor.h>
 #include <ViennaRNA/pair_mat.h>
 #include <ViennaRNA/params/basic.h>
 #include <ViennaRNA/params/constants.h>
 #include <ViennaRNA/params/convert.h>
 #include <ViennaRNA/params/default.h>
-#include <ViennaRNA/params.h>
+// DEPRECATED: #include <ViennaRNA/params.h>
 #include <ViennaRNA/params/io.h>
 #include <ViennaRNA/part_func_co.h>
 #include <ViennaRNA/part_func.h>
 #include <ViennaRNA/part_func_up.h>
 #include <ViennaRNA/part_func_window.h>
 #include <ViennaRNA/perturbation_fold.h>
-#include <ViennaRNA/PKplex.h>
+// DEPRECATED: #include <ViennaRNA/PKplex.h>
 #include <ViennaRNA/plex.h>
-#include <ViennaRNA/plot_aln.h>
-#include <ViennaRNA/plot_layouts.h>
-#include <ViennaRNA/plot_structure.h>
+// DEPRECATED: #include <ViennaRNA/plot_aln.h>
+// DEPRECATED: #include <ViennaRNA/plot_layouts.h>
+// DEPRECATED: #include <ViennaRNA/plot_structure.h>
 #include <ViennaRNA/plotting/alignments.h>
 #include <ViennaRNA/plotting/layouts.h>
-// This was changed to #include <ViennaRNA/plotting/naview/naview.h> in ViennaRNA 2.5.1
-// We'll remove this include for now since #include <ViennaRNA/naview.h> is still available (although deprecated)
-// #include <ViennaRNA/plotting/naview.h>
+#include <ViennaRNA/plotting/naview/naview.h>
 #include <ViennaRNA/plotting/probabilities.h>
 #include <ViennaRNA/plotting/RNApuzzler/RNApuzzler.h>
 #include <ViennaRNA/plotting/RNApuzzler/RNAturtle.h>
 #include <ViennaRNA/plotting/structures.h>
 #include <ViennaRNA/plotting/utils.h>
-#include <ViennaRNA/plot_utils.h>
+// DEPRECATED: #include <ViennaRNA/plot_utils.h>
 #include <ViennaRNA/ProfileAln.h>
 #include <ViennaRNA/profiledist.h>
-#include <ViennaRNA/PS_dot.h>
-#include <ViennaRNA/read_epars.h>
+// DEPRECATED: #include <ViennaRNA/PS_dot.h>
+// DEPRECATED: #include <ViennaRNA/read_epars.h>
 #include <ViennaRNA/ribo.h>
 #include <ViennaRNA/RNAstruct.h>
 #include <ViennaRNA/search/BoyerMoore.h>
 #include <ViennaRNA/sequence.h>
 #include <ViennaRNA/snofold.h>
 #include <ViennaRNA/snoop.h>
-#include <ViennaRNA/stream_output.h>
+// DEPRECATED: #include <ViennaRNA/stream_output.h>
 #include <ViennaRNA/stringdist.h>
-#include <ViennaRNA/string_utils.h>
+// DEPRECATED: #include <ViennaRNA/string_utils.h>
 #include <ViennaRNA/structured_domains.h>
-#include <ViennaRNA/structure_utils.h>
+// DEPRECATED: #include <ViennaRNA/structure_utils.h>
 #include <ViennaRNA/subopt.h>
 #include <ViennaRNA/svm.h>
-#include <ViennaRNA/svm_utils.h>
+// DEPRECATED: #include <ViennaRNA/svm_utils.h>
 #include <ViennaRNA/treedist.h>
-#include <ViennaRNA/units.h>
+// DEPRECATED: #include <ViennaRNA/units.h>
 #include <ViennaRNA/unstructured_domains.h>
 #include <ViennaRNA/utils/alignments.h>
-// Moved this up in front of gquad.h to avoid implicit definition of vrna_alloc()
-// #include <ViennaRNA/utils/basic.h>
+#include <ViennaRNA/utils/basic.h>
 #include <ViennaRNA/utils/cpu.h>
-#include <ViennaRNA/utils.h>
+// DEPRECATED: #include <ViennaRNA/utils.h>
 #include <ViennaRNA/utils/higher_order_functions.h>
 #include <ViennaRNA/utils/strings.h>
 #include <ViennaRNA/utils/structures.h>
 #include <ViennaRNA/utils/svm.h>
 #include <ViennaRNA/utils/units.h>
 #include <ViennaRNA/vrna_config.h>
-#include <ViennaRNA/walk.h>
+// DEPRECATED: #include <ViennaRNA/walk.h>
 #include <ViennaRNA/zscore.h>
 // new since 2.4.18:
 #include <ViennaRNA/pk_plex.h>
